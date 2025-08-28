3a5a1b132f8b7e95122fef2f6781568357499dab
Sala K 301 <salak301@salasfr.itm.edu.co>
2025-08-28 10:06:02 -0500

    commit3

--- Vehiculos3/Program.cs
+++ Vehiculos3/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Vehiculos
+{
+    private string? Marca;
+    private int Precio;
+    private DateTime? FechaSalida; 
+}
+
+public class Carros
+{
+    private string? Modelo;
+    private int Numerollantas;
+ }
+
+public class Motos
+{
+    private int Numerollantas;
+    
+ }
+
+public class Buses
+{
+    private int Numerollantas;
+    private string? Placa;
+    private string? Color;
+ }
